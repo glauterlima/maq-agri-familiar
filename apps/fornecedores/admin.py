328718ac93b3ee0cc77cad6f012d671ed12e8463
Glauter Santos <glauterlima@gmail.com>
2025-07-02 10:27:37 -0300

    Admin Fornecedor configurado

--- apps/fornecedores/admin.py
+++ apps/fornecedores/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.fornecedores.models import Fornecedor
+
+class ListandoFornecedores(admin.ModelAdmin):
+    list_display = ("id", "cnpj", "nome", "ativo")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome" ,)
+    list_editable = ("ativo", )
+    list_per_page = 10
+
+admin.site.register(Fornecedor, ListandoFornecedores)
--- apps/fornecedores/models.py
+++ apps/fornecedores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Fornecedor(models.Model):    
+ 
+    cnpj = models.CharField(max_length=15, null=False, blank=False) 
+    nome = models.CharField(max_length=100, null=False, blank=False) 
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    ativo = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.nome
